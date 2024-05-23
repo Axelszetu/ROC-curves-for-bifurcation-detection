b7cb67bd59afe44ebafff61e06cfb56e3f15c135
axelszetu <axelszetu@gmail.com>
2024-05-23 16:11:03 +0200

    Started investigating Susannes code for parts to use in ROC-curve construction. Made code for evaluating positive rate over a range of values. To be done: generate paths under the null hypothesis.

---
+++ Sketch of ROC-curve implementation.R
@@ -0,0 +1,23 @@
+#Sketch of computing ROC-curves
+#Using variables and data as defined in AMOCestimation.Rmd
+
+#Starting with variance
+no_paths <- dim(var.matrix)[2]
+grid <- seq(from = 0.5, to = 1, by = 0.1)
+thresholds <- qnorm(grid)
+no_thresh <- length(thresholds)
+positive_rates <- numeric(length = no_thresh)
+
+for (i in (1:no_thresh)){
+  thresh <- gam0 + thresholds[i] * sqrt(vargam0)
+  no_triggered <- 0
+  for(j in (1:no_paths)){
+    path <- var.matrix[,j]
+    triggered <- max(path > thresh)
+    no_triggered <- no_triggered + triggered
+  }
+  positive_rates[i] <- no_triggered/no_paths
+}
+
+
+
