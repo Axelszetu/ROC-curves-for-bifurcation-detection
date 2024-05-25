452029b208ec09090aad02a1a051a49989ff251a
axelszetu <axelszetu@gmail.com>
2024-05-25 23:41:02 +0200

    Attempted to compute running estimates of EWS on simulated traces. Still doesnt work.

--- Sketch of ROC-curve implementation.R
+++ Sketch of ROC-curve implementation.R
@@ -1,5 +1,8 @@
 #Sketch of computing ROC-curves
 #Using variables and data as defined in AMOCestimation.Rmd
+library(MASS) 
+library(reshape2) 
+library(reshape) 
 
 #Starting with variance
 no_paths <- dim(var.matrix)[2]
@@ -25,3 +28,23 @@ for (i in (1:no_thresh)){
 #I mean, this one has more than a million rows, so recasting it would give us 10,000 by 1000.
 #The number of cells doesn't increase
 
+#We also need to do some amount of computation on the data:
+#The current data is the raw observations.
+#We need to compute running estimates of var and rho, and that data is to be fed to the positive rates computer
+#I guess Susannes code for computing Var.matrix also takes data of the format we have
+
+EWS.data.H0 <- dcast(data = xx.data.H0, formula = "repetition")
+
+var.matrix.H0 = matrix(-1, ncol = rep, nrow = n - Tw/dt)
+rho.matrix.H0 = matrix(-1, ncol = rep, nrow = n - Tw/dt)
+for(i in 1:(n - Tw/dt)){
+  for(j in 1:rep){
+    data.i = X[i:(i+Tw/dt),j] ## Get data in running window
+    data.i = data.i[data.i > -2.3] ## Remove data if tipped
+    time.i = (1:length(data.i))*dt ## Get time interval for data in running window
+    temp   = lm(data.i ~ time.i)   ## Get linear trend
+    data.i = data.i - temp$fitted.values ## Subtract linear trend
+    var.matrix.H0[i, j] = var(data.i) ## Variance estimate in running window
+    rho.matrix.H0[i, j] = acf(data.i, lag.max = 1, plot = FALSE)$acf[2] ## Autocorrelation estimate in running window
+  }
+}
